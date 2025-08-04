c0bd0183a45bd45f095afa97d9003426e80cc858
Ertanic <black.ikra.14@gmail.com>
2025-08-04 11:32:16 +0300

    feat(server): add axum for routing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,20 @@
+use axum::{Router, routing::get};
+
 mod logs;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let _guard = logs::init_logs();
 
-    tracing::info!("Hello, world!");
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", 3000))
+        .await
+        .expect("Failed to bind port");
+
+    tracing::info!("Listening on http://localhost:3000");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server");
 }
