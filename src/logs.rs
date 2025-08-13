91dead91d503307016e878dfd6a2a54a3ca67b59
Ertanic <black.ikra.14@gmail.com>
2025-08-13 21:52:42 +0300

    feat(server): change log level in debug

--- src/logs.rs
+++ src/logs.rs
@@ -3,6 +3,10 @@ use std::path::PathBuf;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init_logs(config: LogsConfig) -> tracing_appender::non_blocking::WorkerGuard {
+    #[cfg(debug_assertions)]
+    let env = EnvFilter::new("server=trace");
+    
+    #[cfg(not(debug_assertions))]
     let env = if config.debug {
         EnvFilter::new("server=trace")
     } else {
